d79c7bbe4f320298d742114d77da74b1d08204d5
xiazhaowei <xiazhaoweiii@126.com>
2020-08-21 11:56:57 +0800

    “banner”

--- mother/settings.py
+++ mother/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2)#v@%30eyqzbr1(4(42(e89c2*90nt2w*cf62b$22_8%fa%*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
